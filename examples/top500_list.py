b940d2d8e0c048f33e7199b2f88e70db7d0fbe0d
iisaka51@gmail.com <goichiiisaka@GoichiMacBook.local>
2022-07-25 08:22:07 +0900

    update: add example

---
+++ examples/top500_list.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from scraper_tools import Scraper, URL
+
+url = 'https://www.top500.org/lists/top500/2022/06/'
+
+scraper = Scraper()
+response = scraper.request(url)
+top500  = [ x.text.split('\n')
+            for x in response.html.find('table')[0].find('tr') ]
+top500[0] = ['Rank', 'System', 'Onwer', 'Country',
+             'Cores', 'Rmax (PFlop/s)', 'Rpeak (PFlop/s)', 'Power (kW)']
+
+df = pd.DataFrame(top500[1:], columns=top500[0][1:])
+df.to_csv('top500_list.csv')
+
